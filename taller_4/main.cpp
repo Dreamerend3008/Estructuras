da5c973e59e9bf40dd7f3197cc4b919c6f12aef8
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-05-24 18:09:01 -0500

    correcciones a caso en donde agujeros son 0

--- taller_4/main.cpp
+++ taller_4/main.cpp
@@ -9,15 +9,15 @@
 #include <cmath>
 
 int main(int argc, char* argv[]) {
-    if(argc < 2){
-        std::cout << "Uso: " << argv[0] << " <nombre_archivo>" << std::endl;
+    if(argc < 3){
+        std::cout << "Uso: " << argv[0] << " <nombre_archivo> <nombre_archivo_salida>" << std::endl;
         return 1;
     }
     
     std::ifstream archivo(argv[1]);
-    std::ofstream archivo_e("output.txt");
-    if(!archivo.is_open()){
-        std::cerr << "No se pudo abrir el archivo: " << argv[1] << std::endl;
+    std::ofstream archivo_e(argv[2]);
+    if(!archivo.is_open() || !archivo_e.is_open()){
+        std::cerr << "No se pudo abrir uno o ambos archivos." << std::endl;
         return 1;
     }
     std::string linea;
@@ -30,21 +30,25 @@ int main(int argc, char* argv[]) {
         int m = std::stoi(linea);
         archivo_e << m << "\n";
         std::cout<<"El circuito "<<i+1<<" tiene "<<m<<" agujeros \n";
-        std::vector<std::pair<int,int>> agujeros;
+        std::vector<std::pair<double,double>> agujeros;
         for(int j=0;j<m;j++){
             std::getline(archivo, linea);
-            int x, y;
+            double x, y;
             std::stringstream ss(linea);
             ss >> x >> y;
             agujeros.push_back({x, y});
         }
+        if (m == 0) {
+            std::cout << "Distancia total recorrida por el circuito " << i+1 << ": 0\n" << "\n";
+            continue;
+        }
         std::vector<std::pair<int, Nodo>> aristas;
         for(int j=0;j<m;j++){
             for(int k=j+1;k<m;k++){
-                int x1 = agujeros[j].first;
-                int y1 = agujeros[j].second;
-                int x2 = agujeros[k].first;
-                int y2 = agujeros[k].second;
+                double x1 = agujeros[j].first;
+                double y1 = agujeros[j].second;
+                double x2 = agujeros[k].first;
+                double y2 = agujeros[k].second;
                 double w = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
                 aristas.push_back({j, {k, w}});
                 aristas.push_back({k, {j, w}});
@@ -53,8 +57,8 @@ int main(int argc, char* argv[]) {
         int inicial = 0;
         double mn = __DBL_MAX__;
         for(int j=0;j<m;j++){
-            int x = agujeros[j].first;
-            int y = agujeros[j].second;
+            double x = agujeros[j].first;
+            double y = agujeros[j].second;
             double dist = sqrt(pow(x, 2) + pow(y, 2));
             if(dist < mn){
                 mn = dist;
@@ -64,7 +68,7 @@ int main(int argc, char* argv[]) {
         Grafo g(m, aristas);
         std::vector<int> orden = g.ordenar_nodos(inicial);
         
-        int x1=0, y1=0;
+        double x1=0, y1=0;
         double distancia_total = 0.0;
         for(int nodo : orden){
             archivo_e << agujeros[nodo].first << " " << agujeros[nodo].second << "\n";
